b66e9bafa62cc9c166e692b0471fe7f643f5614b
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-09-10 10:28:09 -0500

    imported placeholder

--- src/components/CurrentlyPlaying.jsx
+++ src/components/CurrentlyPlaying.jsx
@@ -2,12 +2,13 @@ import CoverArt from "./CoverArt";
 import SongTitle from "./SongTitle";
 import PlayControls from "./PlayControls";
 import VolumeControl from './VolumeControl';
+import placeholder from './assets/placeholder.svg';
 
 
 export default function CurrentlyPlaying() {
   return (
     <div className="currently-playing w-full p-4 justify-center">
-      <CoverArt />
+      <CoverArt coverImage={placeholder} />
       <SongTitle title="Painted in Blue" artist="Soul Canvas" />
       <PlayControls />
       <VolumeControl />
