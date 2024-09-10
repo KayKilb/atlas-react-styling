423456e3757242574d05321228469e70e893601c
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-09-09 21:35:31 -0500

    CoverArt file was incorrect

--- src/components/CoverArt.jsx
+++ src/components/CoverArt.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const CoverArt = () => {
     return (
         <div
-            classname="w-[436px] h-[422px] absolute top-[-1465px] left-[-274px] gap-0 opacity-0"
-            >
-                <img
-                    src="/src/assets/placeholder.svg"
-                    alt="Cover Art"
-                    className="w-full h-full object-cover"
-                />
-            </div>
+            className="w-[436px] h-[422px] absolute top-[-1465px] left-[-274px] gap-0 opacity-0"
+        >
+            <img
+                src="/src/assets/placeholder.svg"
+                alt="Cover Art"
+                className="w-full h-full object-cover"
+            />
+        </div>
     );
 };
 
