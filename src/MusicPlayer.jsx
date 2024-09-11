d00c6bd4a5e0ec6190ccb82596f0d01601cf4f57
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-09-10 23:04:00 -0500

    adding colors

--- src/MusicPlayer.jsx
+++ src/MusicPlayer.jsx
@@ -4,7 +4,7 @@ import Playlist from './components/Playlist';
 
 const MusicPlayer = () => {
   return (
-    <div className="music-player flex flex-col md:flex-row justify-between gap-8 p-4 bg-primaryPurple-light">
+    <div className="music-player flex flex-col md:flex-row justify-between p-4 gap-8 bg-primaryPurple-light">
       <CurrentlyPlaying />
       <Playlist />
     </div>
