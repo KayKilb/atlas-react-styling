446a8dd9784b322e90b3ee46a8fa2da7de50444e
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-09-10 22:48:24 -0500

    missed component again

--- src/MusicPlayer.jsx
+++ src/MusicPlayer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import CurrentlyPlaying from './components/CurrentlyPlaying';
-import Playlist from './Playlist';
+import Playlist from './components/Playlist';
 
 const MusicPlayer = () => {
   return (
