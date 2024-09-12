7bf432e7837dff5f67086fa1efb00b6665cfd72b
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-09-11 21:51:35 -0500

    color changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="h-full flex flex-col justify-between p-8 min-h-screen">
+    <div className="h-full flex flex-col justify-between p-8 min-h-screen bg-lightPink">
       <MusicPlayer />
       <Footer />
     </div>
